2b4752273162771aa12e4ab4e05e56ee630a475f
Oblum1989 <oscar.blum@gmail.com>
2019-04-04 17:56:11 -0500

    solucion de ejercicio filtrando gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = Expense.where("concept like '%#{params[:concept]}%' AND category_id = '#{params[:category_id]}'")
+    elsif params[:category_id].present?
+      @expenses = Expense.where("category_id = ?", params[:category_id])
+    else
+      @expenses = Expense.order("date DESC")
+    end     
+
   end
 end
