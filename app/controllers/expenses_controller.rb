100e45efb1726d96e167173ca7f07d67fc2f950b
Oblum1989 <oscar.blum@gmail.com>
2019-04-04 20:33:37 -0500

    solucion a reto filtrando gastos 2.0

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,8 +2,10 @@ class ExpensesController < ApplicationController
   def index
     if params[:concept].present? && params[:category_id].present?
       @expenses = Expense.where("concept like '%#{params[:concept]}%' AND category_id = '#{params[:category_id]}'")
-    # elsif params[:category_id].present?
-    #   @expenses = Expense.where("category_id = ?", params[:category_id])
+    elsif params[:concept].present?
+      @expenses = Expense.where("concept like '%#{params[:concept]}%'")
+    elsif params[:category_id].present?
+      @expenses = Expense.where("category_id = ?", params[:category_id])
     else
       @expenses = Expense.order("date DESC")
     end     
