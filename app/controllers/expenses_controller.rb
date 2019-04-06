b4387a8b215def02cb70b6c0c3bc41e0be07e7d2
Oblum1989 <oscar.blum@gmail.com>
2019-04-06 01:44:35 -0500

    se implementa funcionalidad de autenticacion de usuarios

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,5 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
     if params[:concept].present? && params[:category_id].present?
       @expenses = Expense.where("concept like '%#{params[:concept]}%' AND category_id = '#{params[:category_id]}'")
@@ -7,7 +8,7 @@ class ExpensesController < ApplicationController
     elsif params[:category_id].present?
       @expenses = Expense.where("category_id = ?", params[:category_id])
     else
-      @expenses = Expense.order("date DESC")
+      @expenses = Expense.order("date DESC").where("user_id=?",current_user.id)
     end     
 
   end
